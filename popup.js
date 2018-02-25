c6b65d6ae8086b70128ab8fbf04b077de38425ec
iecurius <saahilyechuri@gmail.com>
2018-02-24 22:17:36 -0600

    Fixed ApparentTemp method

--- popup.js
+++ popup.js
@@ -85,10 +85,12 @@ function onLoadFunctions() {
         dayfivetemp += data.list[i].main.temp;
     }
     dayfivetemp = daytwotemp/8;
-function apparantTemp(daytemp, rhperc, windspd) {
+}
+
+function apparentTemp(daytemp, rhperc, windspd) {
     var vp = (rhperc/100)*6.105*Math.Exp(17.27*daytemp/(237.7+daytemp));
-    vat atemp = daytemp + 0.33*vp-0.7*windspd-4.0;
+    var atemp = daytemp + 0.33*vp-0.7*windspd-4.0;
     return atemp;
 }
 
-}
+
