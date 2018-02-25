5f48027cd9f9e3b6b9092b6606ced074e6831db0
iecurius <saahilyechuri@gmail.com>
2018-02-25 09:31:05 -0600

    TEMP CALLS WORK! bitchin'

--- popup.js
+++ popup.js
@@ -20,30 +20,23 @@ function getCurrentTabUrl(callback) {
         // exactly one tab.
         var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+        // A tab is a plain object that provides information about the tab.
+        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+        // tab.url is only available if the "activeTab" permission is declared.
+        // If you want to see the URL of other tabs (e.g. after removing active:true
+        // from |queryInfo|), then the "tabs" permission is required to see their
+        // "url" properties.
+        console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-});
+        callback(url);
+    });
 }
 
 
 window.onload = function onLoadFunctions() {
     var apikey = '6b84a56c561b6aba70eb92a47c27452d';
-    var x = document.getElementById("ua");
-    function getLat(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude;
-    }
-    function getLong(position) {
-        x.innerHTML = "Longitude: " + position.coords.longitude;
-    }
     function getJSON(url) {
         var resp ;
         var xmlHttp ;
@@ -68,35 +61,37 @@ window.onload = function onLoadFunctions() {
         });
     });
 
-    var apiCall = 'http://api.openweathermap.org/data/2.5/weather?lat=' + getLat() + '&lon=' + getLong() + '&appid=' + apikey;
-    var apiCallForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + getLat() + '&lon=' + getLong() + '&appid=' + apikey;
+    var apiCall = 'http://api.openweathermap.org/data/2.5/weather?lat=' + 40.11 + '&lon=' + 88.21 + '&appid=' + apikey;
+    var apiCallForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + 40.11 + '&lon=' + 88.21 + '&appid=' + apikey;
 
-    var data1 = getJSON(apiCall1);
-    var dataForecast = getJSON(apiCallForecast);
-
-    var currdaytemp = data1.main.temp;
+    var data2 = getJSON(apiCall);
+    var data = JSON.parse(data2);
+    var dataForecast2 = getJSON(apiCallForecast);
+    var dataForecast = JSON.parse(dataForecast2);
+    var currdaytemp = data.main.temp;
+    console.log(currdaytemp);
     var daytwotemp = 0;
-    for (i = 1; i <= 7; i++){
-      daytwotemp += data.list[i].main.temp;
+    var dataForecastArray = Array(dataForecast.list);
+    for (i = 0; i <= 6; i++){
+        daytwotemp += dataForecastArray[0][i].main.temp;
+
     }
     daytwotemp = daytwotemp/7;
     var daythreetemp = 0;
-    for (i = 8; i <= 15; i++){
-        daythreetemp += data.list[i].main.temp;
+    for (i = 7; i <= 14; i++){
+        daythreetemp += dataForecastArray[0][i].main.temp;
     }
     daythreetemp = daythreetemp/8;
     var dayfourtemp = 0;
-    for (i = 16; i <= 23; i++){
-        dayfourtemp += data.list[i].main.temp;
+    for (i = 15; i <= 22; i++){
+        dayfourtemp += dataForecastArray[0][i].main.temp;
     }
     dayfourtemp = daytwotemp/8;
     var dayfivetemp = 0;
-    for (i = 24; i <= 31; i++){
-        dayfivetemp += data.list[i].main.temp;
+    for (i = 23; i <= 30; i++){
+        dayfivetemp += dataForecastArray[0][i].main.temp;
     }
     dayfivetemp = daytwotemp/8;
+    console.log()
 }
-
-
-
 
